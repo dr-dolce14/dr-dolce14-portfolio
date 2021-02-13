31be22b092d71005b70b59233d6af8d229b2d447
dr-dolce14 <dolcemascop1@gmail.com>
2021-02-13 11:23:55 -0500

    added link to resume pdf

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const hamburger = document.querySelector('.hamburger')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +11,14 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+window.onscroll = function() {myFunction()};
+
+let sticky = hamburger.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        hamburger.classList.add('sticky')
+    } else {
+        hamburger.classList.remove('sticky')
+    }
+}
